c819b8d263e66d3f9e6e2078cea7bc6f1fd18cb8
Toni <toni.giorgino@gmail.com>
2022-10-05 21:56:00 +0200

    nn

--- gui/alloviz_gui.py
+++ gui/alloviz_gui.py
@@ -3,8 +3,6 @@ import os
 from PyQt5.QtWidgets import *
 import pandas as pd
 
-# os.environ["DYLD_LIBRARY_PATH"]="/opt/homebrew/opt/sqlite/lib/"
-
 sys.path.append("gui")
 # pyuic5 alloviz_main.ui >alloviz_main_ui.py
 from alloviz_main_ui import Ui_MainWindow
@@ -34,29 +32,30 @@ class AlloVizWindow(QMainWindow):
         wlist=AlloViz.AlloViz.info.wrappers
         df = pd.DataFrame(wlist.values())
         df[4]=wlist.keys()
-        df.columns = ["Quantity", "Software", "Metric", "Object", "Keyword"]
+        df.columns = ["Quantity", "Software", "Correlation metric", "Object", "Keyword"]
 
         #df.set_index(["Metric","Quantity","Object"])
         # Rearrange the columns according to the desired nesting
-        df = df[["Metric", "Quantity", "Object", "Software", "Keyword"]]
+        df = df[["Correlation metric", "Quantity", "Object", "Software", "Keyword"]]
 
         tree = self.findChild(QTreeWidget,"methodTree")
         tree.setColumnCount(len(df.columns))
         tree.setHeaderLabels(df.columns)
 
         items = []
+        ditto = "〃"
         for lev1 in df.iloc[:,0].unique():
             lev1_item = QTreeWidgetItem([lev1])
             items.append(lev1_item)
             df1 = df.loc[df.iloc[:,0] == lev1]            
             for lev2 in df1.iloc[:,1].unique():
-                lev2_item = QTreeWidgetItem(["", lev2])
+                lev2_item = QTreeWidgetItem([ditto, lev2])
                 lev1_item.addChild(lev2_item)
                 df2 = df1.loc[df.iloc[:,1]==lev2]
                 for i,leaf in df2.iterrows():
                     leaf_list = list(leaf)
-                    leaf_list[0] = ""
-                    leaf_list[1] = ""
+                    leaf_list[0] = ditto
+                    leaf_list[1] = ditto
                     leaf_item = QTreeWidgetItem(leaf_list)
                     lev2_item.addChild(leaf_item)
         tree.insertTopLevelItems(0, items)
--- gui/run-osx.sh
+++ gui/run-osx.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Some dependency requires Homebrew's sqlite3, while brew does not link it 
+# by default. Add it to the DLL path.
+
 export DYLD_LIBRARY_PATH="/opt/homebrew/opt/sqlite3/lib" 
 python alloviz_gui.py
