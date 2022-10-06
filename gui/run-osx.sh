44a1a6ca80bf8fe33e9efdd2899664cfcca46363
Toni <toni.giorgino@gmail.com>
2022-10-06 13:06:37 +0200

    nn

--- gui/run-osx.sh
+++ gui/run-osx.sh
@@ -3,5 +3,7 @@
 # Some dependency requires Homebrew's sqlite3, while brew does not link it 
 # by default. Add it to the DLL path.
 
+DIRECTORY=$(cd `dirname $0` && pwd)
+
 export DYLD_LIBRARY_PATH="/opt/homebrew/opt/sqlite3/lib" 
-python alloviz_gui.py
+python $DIRECTORY/alloviz_gui.py
