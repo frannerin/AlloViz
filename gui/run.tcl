bfa8b0cbe8df316c0efe7de50d6e9988764c1f70
Toni <toni.giorgino@gmail.com>
2023-01-26 17:22:09 +0100

    runner

--- gui/run.tcl
+++ gui/run.tcl
@@ -1,4 +1,11 @@
 source gui/alloviz_vmd.tcl
-mol new 117/11160_dyn_117.psf
-mol addfile 117/11156_trj_117.dcd waitfor all
+
+# Load sample data if present
+catch {
+    mol new 117/11160_dyn_117.psf
+    mol addfile 117/11156_trj_117.dcd waitfor all
+}
+
+# Simulate the menu action
+alloviz::alloviz_tk
 
